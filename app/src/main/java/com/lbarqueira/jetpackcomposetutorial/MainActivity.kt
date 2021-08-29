bd7354b2ee04209e3be4397a8e8b9a1f1357c684
Luis Barqueira <65919567+lbarqueira@users.noreply.github.com>
2021-08-29 16:57:17 +0100

    Initial commit

---
+++ app/src/main/java/com/lbarqueira/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.lbarqueira.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+data class Message(val author: String, val body: String)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(size = 40.dp)
+                .clip(shape = CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(Message("Colleague", "Hey, take a look at Jackpack compose"))
+}
